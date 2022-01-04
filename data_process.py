2e75b0691e2b02861e959d4b638a88536213f554
CarlJorenz <97073661+CarlJorenz@users.noreply.github.com>
2022-01-04 10:59:16 +0800

    Sorting in categories
    Added Sorting within categories.

--- data_process.py
+++ data_process.py
@@ -1,1 +1,15 @@
-
+#Sorts list based on category and selected criteria
+def sort_in_sort(data_list, category,sorting_method):
+    #Temporary file to store filtered lists
+    temp = []
+    #Filter the list to exclude categories
+    for data in data_list:
+        #Fail safe in cases of categories appearing multiple times or multiple categories.
+        if category.count(data) > 1:
+            temp.append(data)
+    #Changes the sorting method based on the sorting criteria.
+    if sorting_method == 'ascending':
+        temp.sort(key = lambda x: x[1], reverse = True)
+    else:
+        temp.sort(key = lambda x: x[1])
+    return temp
