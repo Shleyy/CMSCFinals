cd636780e6957236a6e8297d6458232ed593fec7
CarlJorenz <97073661+CarlJorenz@users.noreply.github.com>
2022-01-06 22:24:14 +0800

    Update file_mgmt.py

--- file_mgmt.py
+++ file_mgmt.py
@@ -1,14 +1,16 @@
 import glob, os
 from cryptography.fernet import Fernet
 
+#Encoding and Decoding Key
 key = r'pz6Ju_SS4l8rWcBVmSGIPpJymUiPcMF05usXZ-dpOE4='
 
+#Encryption and decryption as bytes instead of strings
 def encrypt(message: bytes, key: bytes) -> bytes:
     return Fernet(key).encrypt(message)
-
 def decrypt(token: bytes, key: bytes) -> bytes:
     return Fernet(key).decrypt(token)
 
+#Checks if .cmsc files exists
 def check_profiles():
     check = glob.glob(r"*.cmsc")
     if check == []:
@@ -16,14 +18,24 @@ def check_profiles():
     else:
         return check
 
-def convert_list_to_string(input_list):
-    end =""
+#Converts lists into a comma separated string
+def list_to_string(input_list):
+    end = ""
     for list1 in input_list:
         list2 = [str(element) for element in list1]
         end += ",".join(list2)
         end += "\n"
     return end
 
+#Converts strings into lists
+def string_to_list(input_list):
+    temp = []
+    split_list = input_list.split('\n')
+    for x in split_list:
+        temp.append(x.split(','))
+    return temp
+
+#Imports csv files as lists
 def import_from_csv(filename):
     filename = filename + '.csv'
     file = open(filename,'r')
@@ -34,7 +46,8 @@ def import_from_csv(filename):
     endlist.pop(0)
     return endlist
 
-def convert_list_to_csv(input_list,profile_name):
+#Exports lists to csv
+def export_to_csv(input_list,profile_name):
     profile_name += '.csv'
     file = open(profile_name,'w')
     temp = ""
@@ -47,9 +60,17 @@ def convert_list_to_csv(input_list,profile_name):
     file.close()
     return end
 
+#Encode lists before writing to cmsc files
 def encode_profile(list1,profile_name):
     global key
     file = open(profile_name+'.cmsc','w')
-    string = convert_list_to_string(list1)
+    string = list_to_string(list1)
     file.write(encrypt(string.encode(), key.encode()).decode())
     file.close()
+
+#Decode profiles into lists
+def decode_profile(file_name):
+    global key
+    file = open(file_name+'.cmsc','r')
+    string = decrypt(file.read().encode(), key.encode()).decode()
+    return list_to_string(string)
